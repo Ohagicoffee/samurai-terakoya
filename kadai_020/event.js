7223999f0801f095c89b66980bcc0aeb0183304a
河原美姫 <158147150+MIKI-KAWAHARA@users.noreply.github.com>
2024-11-20 18:31:37 +0900

    イベント処理内容を定数textに代入

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,8 @@
 //btnというidを持つHTML要素を取得し、定数へ代入
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
 //HTML要素がクリックされたときにイベント処理を実行
 btn.addEventListener('click', ()=> {
-    document.getElementById('text').textContent = "ボタンをクリックしました";
+    text.textContent = "ボタンをクリックしました";
 });
