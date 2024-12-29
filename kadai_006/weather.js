d04801d9262a48599a7f81bc62cc745957918929
河原美姫 <158147150+MIKI-KAWAHARA@users.noreply.github.com>
2024-12-29 19:26:27 +0900

    Delete kadai_006 directory

--- kadai_006/weather.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    //HTML画面が読み込まれたら
-    $(window).on('load', function() {
-        //id要素'target'であるh1タグを'headingクラスを追加して赤色に変更する
-        $('#target').addClass('heading');
-    });
-
-
-});
