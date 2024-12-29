5f292068b449fadbcb7cbf78c0515885a88e7ffe
河原美姫 <158147150+MIKI-KAWAHARA@users.noreply.github.com>
2024-12-29 19:38:08 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-//btnというidを持つHTML要素を取得し、定数へ代入
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-//HTML要素がクリックされたときにイベント処理を実行
-btn.addEventListener('click', ()=> {
-    text.textContent = "ボタンをクリックしました";
-});
