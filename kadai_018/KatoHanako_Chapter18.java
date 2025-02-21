3fd67ea4a83c93b0c5368d6572aeb00a2812dd55
Ohagicoffee <143695958+Ohagicoffee@users.noreply.github.com>
2025-02-21 14:53:54 +0900

    Javaの基礎を学ぼう/ kadai_018をアップロード

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,13 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+	//子クラス自身のコンストラクタを記載
+	KatoHanako_Chapter18() {
+		super("花子");
+	}
+	
+	//親クラスの抽象メソッドを別個の処理を追加
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です");
+	}
+}
