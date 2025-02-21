a5531e6a3acb9a0dfb08d3eb0ece1dd1b7f5f271
Ohagicoffee <143695958+Ohagicoffee@users.noreply.github.com>
2025-02-22 07:50:08 +0900

    Update KatoHanako_Chapter18.java
    setGivenName()を追加

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -3,7 +3,12 @@ package kadai_018;
 public class KatoHanako_Chapter18 extends Kato_Chapter18{
 	//子クラス自身のコンストラクタを記載
 	KatoHanako_Chapter18() {
-		super("花子");
+		super();
+	}
+	
+	//名を表すフィールドの値をセットする
+	public void setGivenName() {
+		this.givenName = "花子";
 	}
 	
 	//親クラスの抽象メソッドを別個の処理を追加
