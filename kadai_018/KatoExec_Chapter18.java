00092eca2c5a3e1aafc1e3d89a92b45390d0d301
Ohagicoffee <143695958+Ohagicoffee@users.noreply.github.com>
2025-02-22 10:28:51 +0900

    Update KatoExec_Chapter18.java
    setGivenNameメソッドを各インスタンスに適用

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -9,8 +9,16 @@ public class KatoExec_Chapter18 {
 		
 		Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
 		
+		//taroの自己紹介
+		taro.setGivenName();
 		taro.execIntroduce();
+		
+		//hanakoの自己紹介
+		hanako.setGivenName();
 		hanako.execIntroduce();
+		
+		//ichiroの自己紹介
+		ichiro.setGivenName();
 		ichiro.execIntroduce();
 
 	}
